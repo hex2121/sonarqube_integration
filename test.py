5478da90459e1335c6408d3d47d98c0070a1e456
Aditya Bhati <aditya@Adityas-MacBook-Air.local>
2025-11-06 18:09:08 +0530

    per changes2

--- test.py
+++ test.py
@@ -2,5 +2,17 @@
 
 
 def test_sum_of_2():
+    sonar_login_key = "sqp_0d6c955b5e9ea6b0ad52eb7c4e3de3a839e900ce"
     sum_of_2 = 1+1
     assert sum_of_2 == 2, f"Failed got {sum_of_2}"
+
+
+def test_sum_of_3():
+    sonar_login_key = "sqp_0d6c955b5e9ea6b0ad52eb7c4e3de3a839e900ce"
+    sum_of_2 = 1+1
+    assert sum_of_2 == 2, f"Failed got {sum_of_2}"
+
+def untested_function():
+    x = 5
+    y = 0
+    return x / y
